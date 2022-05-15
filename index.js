596f1ac6317abe2f58777ea526921e09c3e78b6d
alvaro865 <asuasaca@unsa.edu.pe>
2022-05-15 17:49:40 -0500

    cambio en el servidor, devuelve en json con get

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-app.use(express.static('pub'))
+app.use(express.static('cliente'))
 
 app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000")
